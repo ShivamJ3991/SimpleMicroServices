cb3c5e827b6a2a10b5e5e66d76bb0e3256a55dd0
shivamj3991 <shivamj3991@gmail.com>
2020-07-27 23:23:35 +0530

    "Inital Commit"

---
+++ UserService/Database/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserService.Database
+{
+    public class DatabaseContext : DbContext
+    {
+
+        public DbSet<User> User { get; set; }
+
+       protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            dbContextOptionsBuilder.UseSqlServer(@"Server=.;Database=UserMicorservices;Trusted_Connection=True;");
+        }
+    }
+}
